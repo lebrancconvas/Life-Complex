3dacb3bd102c136dd0e7a5229e2517b81dc6f225
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-19 23:28:52 +0700

    :sparkles: Add Params URL Path.

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.get("/", (req, res, next) => {
     res.send("Hello Express.")
 })
 
-/* app.get("/params/:myparams", (req, res, next) => {
-    res.send("<h1>OK, World.</h1>")
-}) */
+app.get("/params/:myparams", (req, res, next) => {
+    res.send(`Hello ${req.params.myparams} I am ${req.query.weare}`)
+})
 
 app.listen(port, () => {
     console.log(`Server Start at port ${port}`)
